7952ffde95bcc5befaf2a8c86c7ab490176ccedf
Daniel Kai <danielkai@DanieldeMacBook-Air.local>
2023-12-23 18:26:51 +0800

    1223

---
+++ src/library/projectData.js
@@ -0,0 +1,89 @@
+const projectData = [
+  {
+    id:1,
+    title:['HTML','CSS','JavaScript'],
+    list:[
+      {
+        id:1,
+        img:'../images/shoe-img.png',
+        title:'PROJECT',
+        subTitle:'HTML / CSS / JavaScript'
+      },
+      {
+        id:2,
+        img:'../images/pet-img.png',
+        title:'PROJECT',
+        subTitle:'HTML / CSS / JavaScript'
+      },
+      {
+        id:3,
+        img:'../images/dessert-img.png',
+        title:'PROJECT',
+        subTitle:'HTML / CSS / JavaScript'
+      }
+    ]
+  },
+
+
+  {
+    id:2,
+    title:['ReactJS','APP'],
+    list:[
+      {
+        id:1,
+        img:'../images/shoe-img.png',
+        title:'PROJECT',
+        subTitle:'HTML / CSS / JavaScript'
+      },
+      {
+        id:2,
+        img:'../images/pet-img.png',
+        title:'PROJECT',
+        subTitle:'HTML / CSS / JavaScript'
+      }
+    ]
+  },
+
+
+  {
+    id:3,
+    title:['Mobile','APP'],
+    list:[
+      {
+        id:1,
+        img:'../images/shoe-img.png',
+        title:'PROJECT',
+        subTitle:'HTML / CSS / JavaScript'
+      },
+      {
+        id:2,
+        img:'../images/pet-img.png',
+        title:'PROJECT',
+        subTitle:'HTML / CSS / JavaScript'
+      }
+    ]
+  },
+
+
+  {
+    id:4,
+    title:['Graphic','Design'],
+    list:[
+      {
+        id:1,
+        img:'../images/shoe-img.png',
+        title:'PROJECT',
+        subTitle:'HTML / CSS / JavaScript'
+      },
+      {
+        id:2,
+        img:'../images/pet-img.png',
+        title:'PROJECT',
+        subTitle:'HTML / CSS / JavaScript'
+      }
+    ]
+  }
+]
+
+
+export default projectData;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Menu from '../components/Menu'
 import Intro from '../components/Intro'
+import projectData from '../library/projectData.js'
 
 function HomePage() {
   return (
     <>
       <Navbar />
       <Intro/>
-      <Menu/>
-      <Menu/>
-      <Menu/>
-      <Menu/>
+      {
+        projectData.map((item)=>
+        <Menu key={item.id} item={item}/>
+        )
+      }
     </>
   )
 }
